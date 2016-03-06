193534260590fa4c8229433115d1722028f30445
W. Jacob Wagner <w.jacobwagner@gmail.com>
2016-03-06 16:42:31 -0600

    Starting and X-code command line project in C++ to begin porting over Praat's articulatory model.

---
+++ vt-model/vt-model/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  vt-model
+//
+//  Created by William J Wagner on 3/6/16.
+//  Copyright © 2016 Team Jacob. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
