c2b253ed1b58d59f04f2a85f5b4259aefe453f37
W. Jacob Wagner <w.jacobwagner@gmail.com>
2016-03-10 09:24:52 -0600

    Cleaning up tracking of X-Code project files. Also testing classes.

--- vt-model/vt-model/main.cpp
+++ vt-model/vt-model/main.cpp
@@ -7,9 +7,18 @@
 //
 
 #include <iostream>
+#include "Speaker.h"
+#include "Delta.h"
+using namespace std;
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    //Speaker male("Male",2);
+    //cout << male.nose.Dx << "\n";
+    Delta test(40);
+    
+    cout << test.tube->at(0).k1left1 << "\n";
+    
+    cout << "Hello, World!\n";
     return 0;
 }
