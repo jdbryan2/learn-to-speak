8dca4210b4e6882f3ba94c95047c6ba6d8363f3d
W. Jacob Wagner <w.jacobwagner@gmail.com>
2016-03-17 10:11:32 -0500

    Wrote a Sound class for use with port audio to replace the autoSound class in Praat.

---
+++ vt-model/vt-model/Sound.cpp
@@ -0,0 +1,102 @@
+//
+//  Sound.cpp
+//  vt-model
+//
+//  Created by William J Wagner on 3/17/16.
+//  Copyright © 2016 Team Jacob. All rights reserved.
+//
+
+#include "Sound.h"
+#include <assert.h>
+#include <math.h>
+#include <stdio.h>
+
+int Sound::buffer_offset = 0;
+
+Sound::Sound(long _numberOfChannels, float _duration, double _samplingFrequency) {
+    assert(_numberOfChannels>=1 && _numberOfChannels<=2);
+    // TODO: Look up actual acceptable sampling frequency values
+    //assert(_samplingFrequency>=0 && _samplingFrequency<= 1800)
+    assert( _duration>0 && duration<=MAX_DURATION);
+    numberOfChannels = _numberOfChannels;
+    duration = _duration;
+    samplingFrequency = _samplingFrequency;
+    numberOfSamples = round(duration*samplingFrequency);
+    framesPerBuffer = MAX_BUFFER_LEN;
+    z = new float [numberOfSamples];
+}
+
+Sound::~Sound() {
+    delete [] z;
+}
+
+int Sound::play() {
+    PaStream *stream;
+    PaError err;
+    
+    printf("Praat Articulatory Synthesis: Playing speech sound.\n");
+    /* Initialize library before making any other calls. */
+    err = Pa_Initialize();
+    if( err != paNoError ) goto error;
+    
+    /* Open an audio I/O stream. */
+    err = Pa_OpenDefaultStream( &stream,
+                               0,          /* no input channels */
+                               1,          /* stereo output */
+                               paFloat32,  /* 32 bit floating point output */
+                               samplingFrequency,
+                               framesPerBuffer,        /* frames per buffer */
+                               paCallback,
+                               this );
+    if( err != paNoError ) goto error;
+    
+    err = Pa_StartStream( stream );
+    if( err != paNoError ) goto error;
+    
+    /* Sleep for several seconds. */
+    Pa_Sleep((duration+1)*1000);
+    // Reset Buffer Offset
+    buffer_offset = 0;
+    
+    err = Pa_StopStream( stream );
+    if( err != paNoError ) goto error;
+    err = Pa_CloseStream( stream );
+    if( err != paNoError ) goto error;
+    Pa_Terminate();
+    printf("Test finished.\n");
+    return err;
+error:
+    Pa_Terminate();
+    fprintf( stderr, "An error occured while using the portaudio stream\n" );
+    fprintf( stderr, "Error number: %d\n", err );
+    fprintf( stderr, "Error message: %s\n", Pa_GetErrorText( err ) );
+    return err;
+    
+}
+
+
+int Sound::paCallback( const void *inputBuffer, void *outputBuffer,
+                      unsigned long framesPerBuffer,
+                      const PaStreamCallbackTimeInfo* timeInfo,
+                      PaStreamCallbackFlags statusFlags,
+                      void *userData )
+{
+    /* Cast data passed through stream to our structure. */
+    Sound *sound = (Sound*)userData;
+    float *out = (float*)outputBuffer;
+    unsigned int i;
+    (void) inputBuffer; /* Prevent unused variable warning. */
+    int index;
+    
+    for( i=0; i<framesPerBuffer; i++ ) {
+        index = i + buffer_offset;
+        if(index >= sound->numberOfSamples) {
+            *out++ = 0;
+        }
+        else {
+            *out++ = sound->z[index];
+        }
+    }
+    buffer_offset += sound->framesPerBuffer;
+    return 0;
+}
---
+++ vt-model/vt-model/Sound.h
@@ -0,0 +1,39 @@
+//
+//  Sound.h
+//  vt-model
+//
+//  Created by William J Wagner on 3/17/16.
+//  Copyright © 2016 Team Jacob. All rights reserved.
+//
+
+#ifndef Sound_h
+#define Sound_h
+
+#include "portaudio.h"
+
+#define MAX_DURATION 5
+#define MAX_BUFFER_LEN 256
+
+class Sound {
+public:
+    float *z;
+    long numberOfChannels;
+    float duration;
+    float samplingFrequency;
+    int numberOfSamples;
+    unsigned long framesPerBuffer;
+public:
+    Sound(long _numberOfChannels, float _duration, double _samplingFrequency);
+    ~Sound();
+    int play();
+    // TODO: Not sure this variable should be static or not.
+    //       I think it makes sense since we are only playing 1 sound at a time.
+    static int buffer_offset;
+    static int paCallback( const void *inputBuffer, void *outputBuffer,
+                          unsigned long framesPerBuffer,
+                          const PaStreamCallbackTimeInfo* timeInfo,
+                          PaStreamCallbackFlags statusFlags,
+                          void *userData );
+};
+
+#endif /* Sound_h */
