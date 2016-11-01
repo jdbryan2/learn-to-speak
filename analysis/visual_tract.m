39b3fcd0b644ab240dc60e2e23489b56152e2036
Jacob Bryan <jacob.d.bryan@gmail.com>
2016-11-01 17:16:55 -0500

    Visual vocal tract now writes an AVI file but it comes out choppy. Layout cleaned up some too.

--- analysis/visual_tract.m
+++ analysis/visual_tract.m
@@ -25,17 +25,41 @@ hold off
 
 F(loops) = struct('cdata',[],'colormap',[]);
 v = VideoWriter('vocal_tract.avi');
+open(v);
+
+t = 1:64;
+t = t-20;
+lungs = 6:22;
+trachea = 22:35;
+glottis = 35:37;
+tract = 37:64;
+nasal = 64:77;
+
+nasal_vert = max(max(area(:, tract)))+max(max(area(:, nasal)));
+nasal_hor = -13;
 
 for j = 1:loops
-    plot(-21:0, 0.025*area(j*sub_sample, 1:22), '-r')
+    plot(t(lungs), 0.025*area(j*sub_sample, lungs), '-r')
     hold on
-    plot(-21:0,-0.025*area(j*sub_sample, 1:22), '-r')
-    plot(0.5*area(j*sub_sample, 23:89), '-b')
-    plot(-0.5*area(j*sub_sample, 23:89), '-b')
+    plot(t(lungs),-0.025*area(j*sub_sample, lungs), '-r')
+    
+    plot(t(trachea), 0.5*area(j*sub_sample, trachea), '-b')
+    plot(t(trachea), -0.5*area(j*sub_sample, trachea), '-b')
+    
+    plot(t(glottis), 0.5*area(j*sub_sample, glottis), '-g')
+    plot(t(glottis), -0.5*area(j*sub_sample, glottis), '-g')
+    
+    plot(t(tract), 0.5*area(j*sub_sample, tract), '-b')
+    plot(t(tract), -0.5*area(j*sub_sample, tract), '-b')    
+    
+    plot(t(nasal+nasal_hor), 0.5*area(j*sub_sample, nasal)+nasal_vert, '-b')
+    plot(t(nasal+nasal_hor), -0.5*area(j*sub_sample, nasal)+nasal_vert, '-b')
+    
     hold off
     drawnow
     F(j) = getframe(gcf);
+    writeVideo(v, F(j));
 end
-
+close(v);
 fig = figure;
 movie(fig,F,2)
