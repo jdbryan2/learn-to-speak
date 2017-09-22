974a3fb92d973abcfb0976dbeba14f5bef0ed4db
Jacob Bryan <jdbryan2@illinois.edu>
2017-09-22 17:22:58 -0500

    Moved all classes to the same folder because there's no reason not to right now.

---
+++ python/articulatory_learn.py
@@ -0,0 +1,3 @@
+# script for learning primitives based on articulator feature outputs
+# script generates relevant figures detailing the learning of the primitives
+# end of script includes simple control with constant high level input set to 0
---
+++ python/articutory_control.py
@@ -0,0 +1,5 @@
+# load primitives from file
+# control vocal tract 
+# generate figures with constant control inputs at various values
+# save vocal tract data of utterance to file, and save output audio to file
+
