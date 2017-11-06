a60443455f5441054cde7669eea9316684ddec71
W. Jacob Wagner <w.jacobwagner@gmail.com>
2017-11-06 12:42:22 -0600

    Changed install directory of shared libraries and got python to find them using PYTHONPATH env variable. Need to clean up Cmake and merge with Jacob's latest changes now.

--- build.sh
+++ build.sh
@@ -12,7 +12,11 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
     PYTHON_VERSION=`python -c "import sys;t='{v[0]}.{v[1]}'.format(v=list(sys.version_info[:2]));sys.stdout.write(t)";`
     PYTHON_LIBRARY=/usr/local/Frameworks/Python.framework/Versions/$PYTHON_VERSION/lib/libpython$PYTHON_VERSION.dylib
     PYTHON_INCLUDE_DIR=/usr/local/Frameworks/Python.framework/Versions/$PYTHON_VERSION/Headers/
-    cmake -DPYTHON_LIBRARY=$PYTHON_LIBRARY -DPYTHON_INCLUDE_DIR=$PYTHON_INCLUDE_DIR .. && make && make install
+    cmake -DPYTHON_LIBRARY=$PYTHON_LIBRARY -DPYTHON_INCLUDE_DIR=$PYTHON_INCLUDE_DIR .. && make && sudo make install
+    # Make sure python will be able to find the installed shared libraries
+    export PYTHONPATH=/usr/local/PyRAAT/
+    ln -s /usr/local/PyRAAT/Artword.so /usr/local/lib/Artword.so
+    ln -s /usr/local/PyRAAT/PyRAAT.so /usr/local/lib/PyRAAT.so
 elif [[ "$OSTYPE" == "cygwin" ]]; then
     : # POSIX compatibility layer and Linux environment emulation for Windows
 elif [[ "$OSTYPE" == "msys" ]]; then
