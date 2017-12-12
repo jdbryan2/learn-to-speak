b0a6e8eb431acf3f52c911fdc0e0e8eff3c3a0d0
W. Jacob Wagner <w.jacobwagner@gmail.com>
2017-12-11 20:57:27 -0600

    Tried out same learning schedule as I used with 2 states and got working for 1 state. It takes a bit, but the result is that it seems to work for an initial art of zeros. Graphs saved off with sha1.

--- python/art_q_learn.py
+++ python/art_q_learn.py
@@ -52,7 +52,7 @@ primdir = dirname+'_prim'
 
 max_seconds =   5.0
 # RANDOM INIT
-#initial_art=np.random.random((aw.kArt_muscle.MAX, ))
+#initial_art=np.random.random((aw.kArt_muscle.MAX, )) #------
 # STATIC INIT
 # From a randomly generated art that worked well
 """
@@ -61,21 +61,21 @@ initial_art = np.array([ 0.52779292,  0.32185364,  0.86558425,  0.33471684,  0.6
  0.32252938,  0.63632587,  0.50026815,  0.98682582,  0.05327814,  0.57564972,
  0.09049773,  0.92107307,  0.25266528,  0.10626182,  0.59077853,  0.83012719,
  0.65740627,  0.65219443,  0.7657366,   0.66722533,  0.49950773,])
- """
+"""
 # Does well for state 0 with goal btw [.6,1.3)
-#initial_art=np.zeros((aw.kArt_muscle.MAX, ))
+initial_art=np.zeros((aw.kArt_muscle.MAX, ))
 # Does ok for state 1 with goal btw [-2,-1.33) using 1-10.0/(e-exploit_offset+10.0) for exploit
 #initial_art=np.ones((aw.kArt_muscle.MAX, ))
 
 # initialize from ipa 305. see ipa305.py
-
+"""
 initial_art=np.zeros((aw.kArt_muscle.MAX, ))
 initial_art[aw.kArt_muscle.INTERARYTENOID] = 0.5
 initial_art[aw.kArt_muscle.LUNGS] = 0.3
 initial_art[aw.kArt_muscle.MYLOHYOID] = 0.1
 initial_art[aw.kArt_muscle.SPHINCTER] = 0.7
 initial_art[aw.kArt_muscle.HYOGLOSSUS] = 0.3
-
+"""
 
 print "Initial Articulation"
 print initial_art
@@ -96,38 +96,39 @@ num_int_state_bins = 10
 reset_action = 100
 # ensure that actions and states are 2d arrays
 #states = np.linspace(-10.0,10.0,num=num_state_bins)
-#goal_state = 1
-#goal_width = .4
-#states = np.linspace(-2.0,goal_state-goal_width/2.0,num=np.floor(num_state_bins/2.0))
-#states = np.append(states,np.linspace(goal_state+goal_width/2.0,2.0,num=np.ceil(num_state_bins/2.0)))
+goal_state = 1
+goal_width = .2
+states = np.linspace(-2.0,goal_state-goal_width/2.0,num=np.floor(num_state_bins/2.0))
+states = np.append(states,np.linspace(goal_state+goal_width/2.0,2.0,num=np.ceil(num_state_bins/2.0)))
+states = states.reshape(1,states.shape[0])
+# 2DSTATE
+"""
 first_state = np.linspace(-4,2,num=num_state_bins)
 first_state = first_state.reshape(1,first_state.shape[0])
 other_state = np.linspace(-4,2,num=num_state_bins)
 other_state = other_state.reshape(1,other_state.shape[0])
-
-# 2DSTATE
 states = np.concatenate((first_state,other_state),axis=0)
+"""
 
+# INTSTATE
 #action_int_state = np.linspace(-20,20,num=num_int_state_bins)
 #action_int_state = action_int_state.reshape(1,action_int_state.shape[0])
 #prev_state = np.linspace(-1,1,num=num_int_state_bins)
 #prev_state = prev_state.reshape(1,prev_state.shape[0])
-prev_state = states
-
-# INTSTATE
+#prev_state = states
 #states = np.concatenate((states,action_int_state))
 #states = np.concatenate((states,prev_state))
 
 print states
 # 1DSTATE
-#goal_state_index = np.array([np.floor(num_state_bins/2.0)])
+goal_state_index = np.array([np.floor(num_state_bins/2.0)])
 # 2DSTATE and INTSTATE
 #goal_state_index = np.array([np.floor(num_state_bins/2.0),np.floor(num_state_bins/3.0)])
 #goal_state_index = np.array([np.floor(num_state_bins/2.0),-1])
 #goal_state_index = np.array([np.floor(num_state_bins/2.0),np.floor(num_state_bins/3.0),-1,-1])
 #goal_state_index = np.array([7,5])
 #1.2,-1.7 # For IPA 305
-goal_state_index = np.array([8,4])
+#goal_state_index = np.array([8,4])
 print("Goal State")
 print goal_state_index.shape
 # 1DSTATE OR 2DSTATE
@@ -138,11 +139,13 @@ ind2d = np.zeros((2,dim))
 #ind2d[1,0] = goal_state_index
 ind2d[1,:] = goal_state_index
 print states[tuple(ind2d.astype(int))]
+
+#1DACTION
 actions_inc = np.linspace(-1.0,1.0,num=num_action_bins)
 #actions_inc = np.append(actions_inc,reset_action)
 actions_inc = actions_inc.reshape(1,actions_inc.shape[0])
 # 2DACTION
-actions_inc = np.concatenate((actions_inc,actions_inc),axis=0)
+#actions_inc = np.concatenate((actions_inc,actions_inc),axis=0)
 print actions_inc
 
 q_learn = Learner(states = states,
@@ -152,7 +155,7 @@ q_learn = Learner(states = states,
                   alpha = 0.99)
 
 # Perform Q learning Control
-num_episodes = 85
+num_episodes = 80 #----10
 num_view_episodes = 2
 num_tests = 2
 # TODO: Change to condition checking some change between Q functions
@@ -165,27 +168,27 @@ for e in range(num_episodes+num_tests):
     # RANDOM INIT
     #control.InitializeControl(initial_art = np.random.random((aw.kArt_muscle.MAX, )))
     
-    #learning_rate = 0.1
-    #learning_rate = 1.0/(e+1.0)
-    #learning_rate = 10.0/(e+10.0)
-    #learning_rate = 20.0/(e+20.0)
-    learning_offset = 5
+    learning_offset = 5 #------0
     if e<learning_offset:
         learning_rate = 1
     else:
         learning_rate = 20.0/(e-(learning_offset-1)+20.0)
+        #learning_rate = 0.1
+        #learning_rate = 1.0/(e+1.0) #--------
+        #learning_rate = 10.0/(e+10.0)
+        #learning_rate = 20.0/(e+20.0)
 
     print "Learning Rate = " + str(learning_rate)
     
     #1-1.0/(e+1.0)
-    exploit_offset = 10
+    exploit_offset = 10 #----0
     if e<min(exploit_offset,num_episodes):
         exploit_prob = 0
     elif  e >= num_episodes:
         exploit_prob = 1
     else:
         #exploit_prob = 1-1.0/(0.1*e**(1/10.0)+1.0)
-        #exploit_prob = 1-1.0/(0.1e**(1/10.0)+1.0)
+        #exploit_prob = 1-1.0/(e**(1/10.0)+1.0) #------------
         # This worked ish for two states and actions and 20 10 sectiond trials
         #exploit_prob = 1-1.0/(0.02*(e-exploit_offset)+1.0)
         #exploit_prob = 1-1.0/(0.01*(e-exploit_offset)+1.0)
@@ -286,17 +289,16 @@ for e in range(num_episodes+num_tests):
         for k in range(actions_inc.shape[0]):
             plt.plot(control.action_hist[k][0:-1], color=str(0.5+k/(2*actions_inc.shape[0])))
         # 1DSTATE
-        #fig = plt.figure()
-        #pltm.imshow(q_learn.Q)
+        fig = plt.figure()
+        pltm.imshow(q_learn.Q)
         # 2DSTATE and INTSTATE
         #for k in range(actions_inc.shape[1]):
         #    fig = plt.figure()
-#    pltm.imshow(q_learn.Q[:,:,k])
-
+        #    pltm.imshow(q_learn.Q[:,:,k])
+        control.Save()
         pltm.show()
 
 print q_learn.Q
-control.Save()
 #plt.plot(_h)
 savedir = 'data/' + primdir + '/figures/in_out/'
 if not os.path.exists(savedir):
