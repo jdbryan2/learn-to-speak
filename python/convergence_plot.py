417c243a192b603d4d62c6872de9d0a527403203
Jacob Bryan <jdbryan2@illinois.edu>
2018-04-08 17:09:16 -0500

    Convergence plot looks at a normalized error. Also plots average value of F operator.

--- python/convergence_plot.py
+++ python/convergence_plot.py
@@ -15,6 +15,7 @@ if os.path.exists(directory):
 index_list = np.array(sorted(index_list))
 
 error = np.array([])
+mean_F = np.array([])
 for index in index_list:
     print index 
     fname = os.path.join(directory, 'round'+str(index)+'.npz')
@@ -22,12 +23,25 @@ for index in index_list:
     if index == 1:
         old_F = np.zeros(data['F'].shape)
 
-    error = np.append(error, np.sum(np.abs(old_F-data['F'])))
+    
+    error = np.append(error, np.sum(np.abs(old_F-data['F'])/(np.abs(old_F)+np.abs(data['F'])))/data['F'].size)
     old_F = np.copy(data['F'])
+    mean_F = np.append(mean_F, np.mean(data['F']))
+    #plt.figure()
+    #plt.imshow(np.abs(data['F']))
+    #if index % 9 == 0:
+        #plt.show()
     
 
 plt.plot(error)
-plt.title("Change in Predictor Operator")
-plt.ylabel("$ \sum | \Delta F |$")
+plt.title("Normalized Change in Predictor Operator")
+plt.ylabel("$\Delta F$") #"$ \sum | \Delta F |$")
 plt.xlabel("Batch Count (60 sec each)")
+
+plt.figure()
+plt.plot(mean_F)
+plt.title("Average value of element in F")
+plt.ylabel("$F$") #"$ \sum | \Delta F |$")
+plt.xlabel("Batch Count (60 sec each)")
+
 plt.show()
--- python/run_batch.sh
+++ python/run_batch.sh
@@ -1,4 +1,4 @@
-for k in {1..10}
+for k in {1..100}
 do 
     echo Batch $k
     python batch_learn.py |& tee -a data/batch/log.txt
