9a1c679417180633ef84bfec35083096127d6b1f
Jacob Bryan <jdbryan2@illinois.edu>
2018-08-31 21:11:04 -0400

    more play with clusters. Got dbscan working on data set with reasonably short run time.

--- python/mfcc_clusters.py
+++ python/mfcc_clusters.py
@@ -13,11 +13,13 @@ from primitive.DataHandler import DataHandler
 from features.functions import MFCC
 
 from sklearn.neighbors import NearestNeighbors
+from sklearn.cluster import DBSCAN
 
 import pylab as plt
 
 
 FRESH_LOAD = False
+#FRESH_LOAD = True
 directory = "data/rand_steps"
 handler = DataHandler()
 
@@ -61,7 +63,35 @@ else:
 #plt.show()
 #from sklearn.neighbors import kneighbors_graph
 #A = kneighbors_graph(X, 2, mode='connectivity', include_self=True)
+
+# trim off first mfcc because it's mostly just energy
+Y = Y[:, 1:]
+
 nn = NearestNeighbors(radius=1)
 nn.fit(Y)
 A = nn.radius_neighbors_graph(Y, mode='distance')
+
+print 'Clustering'
+db = DBSCAN(eps=0.3, min_samples=5).fit(Y)
+core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
+core_samples_mask[db.core_sample_indices_] = True
+labels = db.labels_
+
+# Number of clusters in labels, ignoring noise if present.
+n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+
+
+print('Estimated number of clusters: %d' % n_clusters_)
+
+Y_ = np.copy(Y[labels==1, :])
+for k in range(20, n_clusters_):
+    Y_ = np.append(Y_, Y[labels==k, :], axis=0)
+    #plt.figure()
+    #plt.imshow(Y[labels==k, :].T, interpolation=None, aspect=np.sum(labels==k)/12.)
+    #plt.title(np.sum(labels==k))
+    #plt.show()
+
+plt.imshow(Y_[:, :].T, interpolation=None, aspect=Y_.shape[0]/12.)
+plt.show()
+
 
