9ca8d2de5f7118d61a1830d44cf3c61e59dcd1b9
Jacob Bryan <jacob.d.bryan@gmail.com>
2018-09-07 16:24:20 -0400

    Reverted CMakeLists so that it can compile on linux machine.

--- python/run_batch.sh
+++ python/run_batch.sh
@@ -1,4 +1,4 @@
-for k in {1..10}
+for k in {1..1}
 do 
     echo Batch $k
     #python batch_learn_mfcc.py --past 1 --future 1 --period 20 --dim 10 --init random >> data/batch_mfcc.txt
@@ -8,6 +8,6 @@ do
 
     #python batch_learn.py --past 20 --future 5 --period 1 --dim 10 --init random >> data/batch_zeros_20_5_1.txt
     #python batch_learn.py --past 12 --future 12 --period 20 --dim 10 --init random >> data/batch_zeros_12_12_20.txt
-    python batch_learn.py --past 20 --future 10 --period 5 --dim 10 --init random >> data/batch_zeros_20_10_5.txt
+    python batch_learn.py --past 20 --future 10 --period 5 --dim 10 --init random #>> data/batch_zeros_20_10_5.txt
     #python batch_learn.py |& tee -a data/batch/log.txt
 done
