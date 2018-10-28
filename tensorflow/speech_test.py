71cc1abb7f1b268b75fab76d8d57db36e014e16c
Jacob Bryan <jdbryan2@illinois.edu>
2018-10-28 10:26:09 -0400

    Trying out VAE with acoustic and articulator values as output (acoustic only for input).

--- tensorflow/speech_test.py
+++ tensorflow/speech_test.py
@@ -6,7 +6,7 @@ from mpl_toolkits.mplot3d import Axes3D
 import scipy.signal as sig
 
 import tensorflow as tf
-from autoencoder.pyraat_vae import VAE, PyraatDataset, LoadData, variable_summaries
+from autoencoder.pyraat_vae import VAE, PyraatDataset2, LoadData, variable_summaries
 
 from genfigures.plot_functions import *
 
@@ -23,8 +23,8 @@ features = (features-min_f)/(max_f - min_f)
 
 plt.show()
 
-d_train = PyraatDataset(actions[:-100, :], features[:-100, :])
-d_val = PyraatDataset(actions[-100:, :], features[-100:, :])
+d_train = PyraatDataset2(actions[:-300, :], features[:-300, :])
+d_val = PyraatDataset2(actions[-300:, :], features[-300:, :])
 #print d_train.num_batches(50)
 
 
@@ -32,7 +32,7 @@ LOAD = True
 TRAIN = True
 EPOCHS = 90
 save_dir = './trained/artnet'
-test_name = 'artnet'
+test_name = 'acoustic_artnet'
 log_dir = save_dir+'/'+test_name+'_logs'
 load_path = save_dir+'/'+test_name+'.ckpt'
 save_path = save_dir+'/'+test_name+'.ckpt'
@@ -42,7 +42,7 @@ if not os.path.exists(save_dir):
 
 
 input_dim, output_dim = d_train.parameter_sizes()
-latent_size = 7
+latent_size = 10
 #print input_size, output_size, state_size 
 
 #from jh_utilities.datasets.unsupervised_dataset import UnsupervisedDataset
@@ -71,7 +71,9 @@ h = model.encode(y)
 
 _x = model.decode(h)
 
+plt.scatter(np.arange(50), np.mean(np.abs(x-_x)**2, axis=1))
+
 #plt.scatter(y, x) 
 
 #plt.scatter(y, _x, c='r')
-#plt.show()
+plt.show()
