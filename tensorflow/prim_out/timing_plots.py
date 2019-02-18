ed04d1fa03efb90e90502190ddbeef81f788402a
Jacob Bryan <jdbryan2@illinois.edu>
2019-02-18 10:23:22 -0500

    Putting together last plots.

--- tensorflow/prim_out/timing_plots.py
+++ tensorflow/prim_out/timing_plots.py
@@ -46,8 +46,8 @@ if os.path.exists(directory):
         std += np.mean(rx_std, axis=0)
         E = np.append(E, error[-1, :].reshape((1,20)), axis=0)
         Y = np.append(Y, np.mean(rx[50:, :], axis=0).reshape((1,20)), axis=0)
-        cum_error = np.cumsum(error, axis=0)
-        for t in range(error.shape[0]):
+        cum_error = np.cumsum(error[20:, :], axis=0)
+        for t in range(cum_error.shape[0]):
             cum_error[t, :] = cum_error[t, :]/(t+1)
 
         #for k in range(cum_error.shape[1]):
@@ -56,7 +56,8 @@ if os.path.exists(directory):
         #    plt.plot(rx[:, k], 'r--')
         #    plt.show()
 
-        mean_error += cum_error
+        mean_error += abs(cum_error)**2
+        #mean_error += np.abs(error)#**2
 
 E = E[1:, :] # remove first row of zeros
 Y = Y[1:, :] # remove first row of zeros
@@ -73,15 +74,44 @@ print np.log2(dE)
 std = std/total_samples
 plt.plot(std)
 plt.show()
-pl.imshow(np.abs(cov_E))
+plt.imshow(np.abs(cov_E))
 plt.show()
 plt.imshow(np.abs(cov_Y))
 plt.show()
 print cov_E.shape
-mean_error = mean_error/total_samples
+mean_error = mean_error/total_samples-3.2
 
-for k in range(mean_error.shape[1]):
-    if std[k] < 0.06:
-        plt.plot(np.abs(mean_error[:, k]))
+ind = np.argsort(abs(mean_error[-1, :]))
 
-plt.show()
+rows = np.arange(mean_error.shape[1])
+plt.figure()
+PlotTraces(np.abs(mean_error).T, rows, max_length=mean_error.shape[0], sample_period=80, highlight=0)
+#plt.plot([0.2, 0.2], [0, 1], 'r--')
+plt.xlabel("Time (s)")
+plt.ylabel("Mean Square Error")
+plt.ylim([0, 1])
+tikz_save(directory+'/timing_error.tikz',
+            data_path='tikz/ICE/')
+plt.close()
+#plt.show()
+
+print mean_error[-1, ind]
+print np.log2(1./mean_error[-1, ind])
+print np.cumsum(np.log2(1./mean_error[-1, ind]))
+
+#for k in rows:
+#    print k
+#    plt.figure()
+#    PlotTraces(np.abs(mean_error).T, rows, max_length=mean_error.shape[0], sample_period=80, highlight=k)
+#    plt.title(k)
+#plt.show()
+
+#for k in range(mean_error.shape[1]):
+#for k, dex in enumerate(ind):
+#    #if std[k] < 0.06:
+#    if k>10:
+#        plt.plot(np.abs(mean_error[:, dex]), '--b', alpha=0.3)
+#    else:
+#        plt.plot(np.abs(mean_error[:, dex]), 'b')
+#
+#plt.show()
