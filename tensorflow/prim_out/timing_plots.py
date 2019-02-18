718f70757788b225b9ef2fa7cec3a5793787bbe4
Jacob Bryan <jdbryan2@illinois.edu>
2019-02-18 14:33:10 -0500

    tensorflow/prim_out/reproduction_error.py

--- tensorflow/prim_out/timing_plots.py
+++ tensorflow/prim_out/timing_plots.py
@@ -26,6 +26,7 @@ total_samples = 0
 mean_error = 0
 E = np.zeros((1, 20))
 Y = np.zeros((1, 20))
+X = np.zeros((1, 20))
 std = 0
 
 if os.path.exists(directory):
@@ -46,6 +47,7 @@ if os.path.exists(directory):
         std += np.mean(rx_std, axis=0)
         E = np.append(E, error[-1, :].reshape((1,20)), axis=0)
         Y = np.append(Y, np.mean(rx[50:, :], axis=0).reshape((1,20)), axis=0)
+        X = np.append(X, np.mean(tx[50:, :], axis=0).reshape((1,20)), axis=0)
         cum_error = np.cumsum(error[20:, :], axis=0)
         for t in range(cum_error.shape[0]):
             cum_error[t, :] = cum_error[t, :]/(t+1)
@@ -56,14 +58,16 @@ if os.path.exists(directory):
         #    plt.plot(rx[:, k], 'r--')
         #    plt.show()
 
-        mean_error += abs(cum_error)**2
+        mean_error += cum_error
         #mean_error += np.abs(error)#**2
 
 E = E[1:, :] # remove first row of zeros
+X = X[1:, :] # remove first row of zeros
 Y = Y[1:, :] # remove first row of zeros
 
 cov_E  = np.cov(E.T)
 cov_Y  = np.cov(Y.T)
+cov_X = np.cov(X.T)
 dE = np.linalg.det(cov_E)
 dY = np.linalg.det(cov_Y)
 I = np.log2(dY/dE)
@@ -72,33 +76,77 @@ print np.log2(dY)
 print np.log2(dE)
 
 std = std/total_samples
-plt.plot(std)
+#plt.plot(std)
+#plt.show()
+#plt.imshow(np.abs(cov_E))
+#plt.show()
+#plt.imshow(np.abs(cov_Y))
+#plt.show()
+cov_E /= 4.
+cov_X /= 4.
+lamb, Q = np.linalg.eig(cov_E)
+lamby, Qy = np.linalg.eig(cov_Y)
+#lamb = lamb-0.4
+print lamb
+print lamby
+A  = np.dot(Q.T, np.dot(cov_Y, Q))
+plt.imshow(np.abs(A))
 plt.show()
-plt.imshow(np.abs(cov_E))
+
+#print np.log2(np.linalg.det(cov_X+cov_E)/np.linalg.det(cov_E))
+le = lamb[lamby>lamb]
+ly = lamby[lamby>lamb]
+print np.log2(np.product(ly)/np.product(le)), np.log2(np.product(le)), np.log2(np.product(ly))
+
+A = np.dot(Q.T, np.dot(cov_X, Q))
+v = np.zeros(A.shape[0])
+a = np.zeros(A.shape[0])
+x = np.zeros(A.shape[0])
+e = np.zeros(A.shape[0])
+for k in range(A.shape[0]):
+    #print cov_X[k,k]
+    if A[k, k]> 0:
+        v[k] = A[k,k] + lamb[k]
+        a[k] = A[k,k]
+        e[k] = lamb[k]
+        x[k] = cov_X[k,k]
+
+v = v[v>0]
+e = e[e>0]
+print 'v', np.product(v)
+print 'e', np.product(e)
+
+#plt.plot(v)
+plt.plot(a)
+plt.plot(lamb)
+plt.plot(lamby)
 plt.show()
-plt.imshow(np.abs(cov_Y))
-plt.show()
+
 print cov_E.shape
-mean_error = mean_error/total_samples-3.2
+mean_error = mean_error/total_samples
+
+mean_error = np.delete(mean_error, [8,9], 1)
 
 ind = np.argsort(abs(mean_error[-1, :]))
 
 rows = np.arange(mean_error.shape[1])
-plt.figure()
-PlotTraces(np.abs(mean_error).T, rows, max_length=mean_error.shape[0], sample_period=80, highlight=0)
-#plt.plot([0.2, 0.2], [0, 1], 'r--')
-plt.xlabel("Time (s)")
-plt.ylabel("Mean Square Error")
-plt.ylim([0, 1])
-tikz_save(directory+'/timing_error.tikz',
-            data_path='tikz/ICE/')
-plt.close()
+# # generate error plot for dissertation
+#plt.figure()
+#PlotTraces(np.abs(mean_error).T, rows, max_length=mean_error.shape[0], sample_period=80, highlight=0)
+##plt.plot([0.2, 0.2], [0, 1], 'r--')
+#plt.xlabel("Time (s)")
+#plt.ylabel("Mean Square Error")
+#plt.ylim([0, 1])
+#tikz_save(directory+'/timing_error.tikz',
+#            data_path='tikz/ICE/')
+#plt.close()
 #plt.show()
 
-print mean_error[-1, ind]
-print np.log2(1./mean_error[-1, ind])
-print np.cumsum(np.log2(1./mean_error[-1, ind]))
+#print mean_error[-1, ind]
+#print np.log2(1./np.abs(mean_error[-1, ind]))
+#print np.cumsum(np.log2(1./np.abs(mean_error[-1, ind])))
 
+#
 #for k in rows:
 #    print k
 #    plt.figure()
